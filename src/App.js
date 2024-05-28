44f1153fe5d65aa8719e5101eb7f3e76765700e0
Miriam Wahito Maina <miriammaina51@gmail.com>
2024-05-28 16:09:23 +0300

    Created Balance, Header and income expenses

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { IncomeExpenses } from "./components/IncomeExpenses";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+      </div>
+    </div>
+  );
 }
 
 export default App;
